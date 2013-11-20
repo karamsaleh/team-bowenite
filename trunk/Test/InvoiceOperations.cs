230af9121923058ab70af2d6e6a4ffb2e2115d1d
sstoyanov87@mail.bg <sstoyanov87@mail.bg@cda43672-4668-2ec7-ec3a-fa54daf12834>
2013-11-20 16:32:39 +0000

    added a few basic classes

---
+++ trunk/Test/InvoiceOperations.cs
@@ -0,0 +1,16 @@
+﻿namespace Test
+{
+    using System;
+
+    public class InvoiceOperations : Operations
+    {
+        public InvoiceOperations(DateTime date) : base(date)
+        {
+        }
+
+        public override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ trunk/Test/Operations.cs
@@ -0,0 +1,16 @@
+﻿namespace Test
+{
+    using System;
+
+    public abstract class Operations
+    {
+        public DateTime Date { get; protected set; }
+        
+        protected Operations(DateTime date)
+        {
+            this.Date = date;
+        }
+
+        public abstract void Validate();
+    }
+}
