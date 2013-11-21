a3bb884817f0db168594effe955ea9c341b65813
nigetaltd@gmail.com <nigetaltd@gmail.com@cda43672-4668-2ec7-ec3a-fa54daf12834>
2013-11-21 09:27:07 +0000

    ArticleChoice.xaml added

---
+++ trunk/Test/ArticleChoice.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for ArticleChoice.xaml
+    /// </summary>
+    public partial class ArticleChoice : Window
+    {
+        public ArticleChoice()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLostWindowFocus(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.ArticleDetails.Visibility = Visibility.Visible;
+        }
+    }
+}
