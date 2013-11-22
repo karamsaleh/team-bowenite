4e4d0ef1268b73ec49e7cd2c18dcf9ea8fa8f95f
nigetaltd <nigetaltd@gmail.com>
2013-11-22 12:52:03 +0000

    PurchaseItemChoice.xaml added and fixed Purchase page.

---
+++ Test/PurchaseItemChoice.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for PurchaseItemChoice.xaml
+    /// </summary>
+    public partial class PurchaseItemChoice : Window
+    {
+        public PurchaseItemChoice()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLostWindowFocus(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnChoiceButtonClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.ArticleDetails.Visibility = Visibility.Visible;
+        }
+
+        private void OnCloseClickButton(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnAddNewButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window addNewPurchaseItemWindow = new AddNewPurchaseItem();
+            addNewPurchaseItemWindow.Show();
+            this.Close();
+        }
+
+        private void OnCancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.ArticleDetails.Visibility = Visibility.Collapsed;
+        }
+    }
+}
