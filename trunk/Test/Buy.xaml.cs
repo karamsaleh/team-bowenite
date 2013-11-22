c4c70a01682d0ba526f183eefabe507d161a3764
nigetaltd@gmail.com <nigetaltd@gmail.com@cda43672-4668-2ec7-ec3a-fa54daf12834>
2013-11-22 11:26:41 +0000

    Buy page and Design updated

--- trunk/Test/Buy.xaml.cs
+++ trunk/Test/Buy.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,51 @@ namespace Test
         public Buy()
         {
             InitializeComponent();
+            this.DateTextBox.Text = DateTime.Now.ToShortDateString();
+            if (LicenseManager.UsageMode != LicenseUsageMode.Designtime)
+            {
+                this.Width = double.NaN; ;
+                this.Height = double.NaN; ;
+            }
+        }
+
+        private void OnChooseSellerButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window sellerWindow = new SellerChoice();
+            sellerWindow.Show();
+        }
+
+        private void ShowPurchases(object sender, RoutedEventArgs e)
+        {
+            Window showSells = new ShowSells();
+            showSells.Show();
+        }
+
+        private void ShowCalendar(object sender, MouseButtonEventArgs e)
+        {
+            this.ChooseDateCalendar.Visibility = Visibility.Visible;
+        }
+
+        private void OnClickExitButton(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to exit?", "Exit Confirmation", MessageBoxButton.OKCancel);
+            if (result == MessageBoxResult.OK)
+            {
+                Environment.Exit(1);
+            }
+        }
+
+        private void OnArticleButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window window = new ArticleChoice();
+            window.Show();
+        }
+
+        private void ChooseDate(object sender, MouseButtonEventArgs e)
+        {
+            this.ChooseDateCalendar.Visibility = Visibility.Collapsed;
+            var date = sender;
+            this.DateTextBox.Text = date.ToString().Remove(9, 9);
         }
     }
 }
---
+++ trunk/Test/SellerChoice.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for SellerChoice.xaml
+    /// </summary>
+    public partial class SellerChoice : Window
+    {
+        public SellerChoice()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCloseClickButton(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnCancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.ArticleDetails.Visibility = Visibility.Collapsed;
+        }
+
+        private void OnAddNewButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window addNewClientWindow = new AddNewClient();
+            addNewClientWindow.Show();
+            this.Close();
+        }
+    }
+}
