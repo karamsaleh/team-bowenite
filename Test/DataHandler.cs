fb8e303dff0517930f0bf918a132943770ac685a
sstoyanov87 <sstoyanov87@mail.bg>
2013-11-25 10:20:47 +0000

    Removed Stock.cs and did some code formatting

--- Test/DataHandler.cs
+++ Test/DataHandler.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Test
+﻿namespace Test
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
     public class DataHandler
     {
         private static DataHandler instance;
